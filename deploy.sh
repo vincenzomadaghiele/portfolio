5d9bcf1b4774b09874ee65ac551368f77a89b7a0
Vincenzo Madaghiele <vincenzo.madaghiele@gmail.com>
2021-04-12 18:44:04 +0200

    changed website name

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:vincenzomadaghiele/my-website.git master:gh-pages
+git push -f git@github.com:vincenzomadaghiele/portfolio.git master:gh-pages
 cd -
